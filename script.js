657ca97afefaee8afcb10d50acb727cb2d7a7f17
jahuer <cjahuer@gmail.com>
2025-06-08 19:06:01 -0400

    08/06/2025

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
-var swiper = new Swiper(".mySwiper-1", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+var swiper1 = new Swiper(".mySwiper-1", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
 
-var swiper = new Swiper(".mySwiper-2", {
-  slidesPerView: 3,
-  spaceBetween: 20,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
+var swiper2 = new Swiper(".mySwiper-2", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-    520: {
-      slidesPerView: 2,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
     },
-    950: {
-      slidesPerView: 3,
-    },
-  },
 });
 
 let tabInputs = document.querySelectorAll(".tabInput");
 
-tabInputs.forEach(function(input) {
-  input.addEventListener('change', function() {
-    let id = input.ariaValueMax;
-    let thisSwiper = document.getElementById('swiper' + id);
-    thisSwiper.swiper.update();
-  })
+tabInputs.forEach(function (input) {
+    input.addEventListener('change', function () {
+        let id = input.value;
+        let thisSwiper = document.getElementById('swiper' + id);
+
+        if (thisSwiper) {
+            thisSwiper.swiper.update();
+        }
+    });
 });
